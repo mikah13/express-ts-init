113b37b8c927eca1782abe80830682be38f62d53
mikah13 <anhminhhoang13@gmail.com>
2023-11-02 23:26:51 -0700

    added middleware to app

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Express } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import compression from 'compression';
+
+
+
+const app: Express = express();
+
+export default app;
