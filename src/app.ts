85cd5cd00abdfaad736c2c7686d51b6d60a41962
mikah13 <anhminhhoang13@gmail.com>
2023-11-02 23:29:07 -0700

    added: helmet and compression

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,16 @@ import cors from 'cors';
 import helmet from 'helmet';
 import compression from 'compression';
 
-
-
 const app: Express = express();
+
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+app.use(compression());
 
 export default app;
