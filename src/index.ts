22ed5a2cead6f917d251c2b8a062c0876380f0f2
mikah13 <anhminhhoang13@gmail.com>
2023-11-05 00:55:33 -0700

    add: logger methods

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 import app from './app';
+import config from './config/config';
+const server = app.listen(parseInt(config.port), () => {
+  logger.log('info', `Server is running on Port: ${config.port}`);
+});
--- src/middlware/logger.ts
+++ src/middlware/logger.ts
@@ -1,7 +1,20 @@
-import winston from 'winston';
-import config from '@/config/config';
-const logger = winston.createLogger({
+import { createLogger, format, transports } from 'winston';
+import config from '../config/config';
+const logger = createLogger({
   level: config.node_env === 'production' ? 'info' : 'debug',
-  format: winston.format.json(),
-  transports: [new winston.transports.Console()],
+  format: format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.File({ filename: 'logs/error.log', level: 'error' }),
+    new transports.File({ filename: 'logs/status.log' }),
+  ],
+  exceptionHandlers: [new transports.File({ filename: 'logs/exceptions.log' })],
 });
+if (config.node_env !== 'production') {
+  logger.add(
+    new transports.Console({
+      format: format.simple(),
+    })
+  );
+}
+export default logger;
